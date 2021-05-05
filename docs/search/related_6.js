d2b9a7c2b4fc9287bad383389cda2ee472aaab75
abitmore <abitmore@users.noreply.github.com>
2021-05-05 15:42:20 +0000

    Update to 5.2.1

--- docs/search/related_6.js
+++ docs/search/related_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['account_5fhistory_5fplugin',['account_history_plugin',['../classgraphene_1_1account__history_1_1detail_1_1account__history__plugin__impl.html#ae8a46ca2d211f59cd1f874b7c583927e',1,'graphene::account_history::detail::account_history_plugin_impl']]],
+  ['application',['application',['../classgraphene_1_1app_1_1detail_1_1application__impl.html#ae70923dc6efde4f0fa3c4fd312607cef',1,'graphene::app::detail::application_impl']]],
   ['custom_5foperations_5fplugin',['custom_operations_plugin',['../classgraphene_1_1custom__operations_1_1detail_1_1custom__operations__plugin__impl.html#aab6af820072b2449aa449e7935fcff35',1,'graphene::custom_operations::detail::custom_operations_plugin_impl']]],
   ['template_5fplugin',['template_plugin',['../classgraphene_1_1template__plugin_1_1detail_1_1template__plugin__impl.html#a4b98acc13d66a55b9a11755a3f5a2b69',1,'graphene::template_plugin::detail::template_plugin_impl']]]
 ];
